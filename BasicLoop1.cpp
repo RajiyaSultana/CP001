135eedcfc4bd01d1ebc64fd2a38c212b10ae4d39
Rajiya Sultana <rssumu04@gmail.com>
2023-11-13 23:00:06 +0600

    Add comments

--- BasicLoop1.cpp
+++ BasicLoop1.cpp
@@ -3,15 +3,15 @@ using namespace std;
 
 int main()
 {
-   int n,h=0, m = 0, s=0;
-   cin >> n;
+   int n,h=0, m = 0, s=0; 
+   cin >> n; //getting input;
 
-   h = n/3600;
-   n = n % 3600;
-   m = n / 60;
-   s = n % 60;
+   h = n/3600; // 1 hour = 3600 sec
+   n = n % 3600; 
+   m = n / 60; // 1 min = 60 sec
+   s = n % 60; // after deviding minut
 
-   cout <<h<<":"<<m<<":"<<s <<endl;
+   cout <<h<<":"<<m<<":"<<s <<endl;// output (h:m:n)
 
     return 0;
 } 
